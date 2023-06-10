4af55651a6ac91755eabc7490fa7b0c47091c888
Adam Gerber <adam.gerber@yahoo.com>
2023-06-10 14:54:34 -0500

    add static constants

---
+++ app/src/main/java/edu/uchicago/gerber/capsquiz/model/Constants.kt
@@ -0,0 +1,10 @@
+package edu.uchicago.gerber.capsquiz.model
+
+object Constants {
+
+    //because we define this as "object" this is now a singleton and all of its members are static
+    const val PIPE = "|"
+    const val COUNTRY_INDEX = 0
+    const val CAPITAL_INDEX = 1
+    const val REGION_INDEX = 2
+}
