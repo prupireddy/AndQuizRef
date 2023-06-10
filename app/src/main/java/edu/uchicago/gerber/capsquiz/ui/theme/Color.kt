5489d4048f86ebc94eca24f4df127e40d7f288dc
Adam Gerber <adam.gerber@yahoo.com>
2023-06-10 13:33:41 -0500

    add some colors

--- app/src/main/java/edu/uchicago/gerber/capsquiz/ui/theme/Color.kt
+++ app/src/main/java/edu/uchicago/gerber/capsquiz/ui/theme/Color.kt
@@ -9,3 +9,6 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RedColor = Color(0xFFF44336)
+val Teal200 = Color(0xFF03DAC5)
