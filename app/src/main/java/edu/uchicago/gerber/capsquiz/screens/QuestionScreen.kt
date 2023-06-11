1f2f80eb7be36a6d8acf31ecbccb630bdeaf6d7d
Adam Gerber <adam.gerber@yahoo.com>
2023-06-11 11:34:01 -0500

    QuestionScreen part2

--- app/src/main/java/edu/uchicago/gerber/capsquiz/screens/QuestionScreen.kt
+++ app/src/main/java/edu/uchicago/gerber/capsquiz/screens/QuestionScreen.kt
@@ -1,6 +1,5 @@
 package edu.uchicago.gerber.capsquiz.screens
 
-
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.*
@@ -18,48 +17,75 @@ import edu.uchicago.gerber.capsquiz.ui.theme.RedColor
 fun QuestionScreen() {
 
 
+    val selectedOption = "Warsaw"
+    val answers = listOf("London", "Berlin", "Prague", "Warsaw", "Madrid")
+
 
     Scaffold(
         //define the padding at the root
         Modifier.padding(paddingValues = PaddingValues(all = 10.dp)),
         topBar = {
-        TopAppBar(
-            title = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Capitals Quiz",
+                        textAlign = TextAlign.Start,
+                    )
+                },
+            )
+        }) {
+            paddingValues ->
+        Column(modifier = Modifier
+            .padding(paddingValues = paddingValues)
+            .fillMaxSize()) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(7f)
+            ) {
                 Text(
-                    text = "Capitals Quiz",
-                    textAlign = TextAlign.Start,
+                    text = "Question 1 :",
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                    style = MaterialTheme.typography.subtitle2
                 )
-            },
-        )
-    }) {
-        paddingValues ->
-            Column(modifier = Modifier
-                .padding(paddingValues = paddingValues)
-                .fillMaxSize()) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(7f)
-                ) {
-                    Text(
-                        text = "Question 1 :",
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
-                        style = MaterialTheme.typography.subtitle2
-                    )
-                    Divider()
-                    Text(
-                        text = "What is the capital of Poland?",
-                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
-                        style = MaterialTheme.typography.h6
-                    )
+                Divider()
+                Text(
+                    text = "What is the capital of Poland?",
+                    modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                    style = MaterialTheme.typography.h6
+                )
+                answers.forEach { option ->
+                    Box(modifier = Modifier
+                        .padding(10.dp)
+                        .selectable(
+                            selected = (option == selectedOption),
+                            onClick = { }
+                        )) {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(vertical = 8.dp)
 
+                        ) {
+                            RadioButton(selected = selectedOption == option, onClick = {
+
+                            })
+                            Text(
+                                text = option,
+                                modifier = Modifier
+                                    .padding(horizontal = 8.dp, vertical = 12.dp),
+
+                                style = MaterialTheme.typography.subtitle1
+                            )
+                        }
+                    }
                 }
-
-
-
             }
+            //add row
 
         }
+
+    }
 
 
 
