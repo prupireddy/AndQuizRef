2a79942183e8d95108c6c28c5a3d3bf3de9b3ae3
Adam Gerber <adam.gerber@yahoo.com>
2023-06-11 11:26:39 -0500

    Create partial home screen

--- app/src/main/java/edu/uchicago/gerber/capsquiz/MainActivity.kt
+++ app/src/main/java/edu/uchicago/gerber/capsquiz/MainActivity.kt
@@ -4,43 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import edu.uchicago.gerber.capsquiz.ui.theme.CapsQuizTheme
+import edu.uchicago.gerber.capsquiz.screens.HomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CapsQuizTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            HomeScreen()
         }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    CapsQuizTheme {
-        Greeting("Android")
-    }
+fun DefaultPreview() {
+    HomeScreen()
 }
--- app/src/main/java/edu/uchicago/gerber/capsquiz/screens/HomeScreen.kt
+++ app/src/main/java/edu/uchicago/gerber/capsquiz/screens/HomeScreen.kt
@@ -1,1 +1,86 @@
 package edu.uchicago.gerber.capsquiz.screens
+
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.uchicago.gerber.capsquiz.ui.theme.RedColor
+
+
+@Composable
+fun HomeScreen() {
+
+    Scaffold(
+        //define the padding at the root
+        Modifier.padding(paddingValues = PaddingValues(all = 10.dp)),
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Capitals Quiz",
+                        textAlign = TextAlign.Start,
+                    )
+                },
+            )
+        }) {
+        //pass the paddingValues from Scaffold into the children to be used
+            paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues = paddingValues)
+        ) {
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Text(
+                text = "Welcome to Capitals Quiz",
+                style = MaterialTheme.typography.h5
+            )
+            Spacer(modifier = Modifier.height(40.dp))
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                Text(
+                    text = "Your Name",
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    style = MaterialTheme.typography.body1
+                )
+                OutlinedTextField(
+                    value = "Adam",
+                    onValueChange = {},
+                    modifier = Modifier
+                        .background(Color.Transparent)
+                        .fillMaxWidth(),
+                    keyboardOptions = KeyboardOptions(
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {}
+                    ),
+                    maxLines = 1
+                )
+            }
+
+        }
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
