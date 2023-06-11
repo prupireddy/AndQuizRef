9fc1dfe247f75961940d334ee9e0c3b0174f64d9
Adam Gerber <adam.gerber@yahoo.com>
2023-06-11 11:32:29 -0500

    QuestionScreen part1

--- app/src/main/java/edu/uchicago/gerber/capsquiz/screens/QuestionScreen.kt
+++ app/src/main/java/edu/uchicago/gerber/capsquiz/screens/QuestionScreen.kt
@@ -1,1 +1,72 @@
 package edu.uchicago.gerber.capsquiz.screens
+
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import edu.uchicago.gerber.capsquiz.ui.theme.RedColor
+
+
+@Composable
+fun QuestionScreen() {
+
+
+
+    Scaffold(
+        //define the padding at the root
+        Modifier.padding(paddingValues = PaddingValues(all = 10.dp)),
+        topBar = {
+        TopAppBar(
+            title = {
+                Text(
+                    text = "Capitals Quiz",
+                    textAlign = TextAlign.Start,
+                )
+            },
+        )
+    }) {
+        paddingValues ->
+            Column(modifier = Modifier
+                .padding(paddingValues = paddingValues)
+                .fillMaxSize()) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(7f)
+                ) {
+                    Text(
+                        text = "Question 1 :",
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                        style = MaterialTheme.typography.subtitle2
+                    )
+                    Divider()
+                    Text(
+                        text = "What is the capital of Poland?",
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                        style = MaterialTheme.typography.h6
+                    )
+
+                }
+
+
+
+            }
+
+        }
+
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuestionScreenPreview() {
+    QuestionScreen()
+}
